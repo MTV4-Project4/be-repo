60fa14856ea9df66fa03ceeffb59a19637a0c9d8
gsh118 <hks01108@naver.com>
2024-11-14 22:51:04 +0900

    #19 유사도 챌린지 유저 참가 정보 구현
    유저 참가 정보 db 및 api 구현, ai와 통신으로 유사도 판정 결과와 사진 저장

---
+++ src/main/java/com/walkers/sportslight/userMotionChallenge/command/domain/infrastructure/service/UserMotionFileUploadServiceImpl.java
@@ -0,0 +1,29 @@
+package com.walkers.sportslight.userMotionChallenge.command.domain.infrastructure.service;
+
+import com.walkers.sportslight.common.FileUploadService;
+import com.walkers.sportslight.userMotionChallenge.command.domain.service.UserMotionFileUploadService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class UserMotionFileUploadServiceImpl implements UserMotionFileUploadService {
+
+    private FileUploadService fileUploadService;
+
+    public UserMotionFileUploadServiceImpl(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+    @Override
+    public String uploadFile(MultipartFile file) throws IOException {
+        return fileUploadService.fileUpload("motion/user/"+file.getOriginalFilename(), file);
+    }
+
+    @Override
+    public void deleteFile(String filePath) throws IOException {
+        fileUploadService.deleteFile(filePath);
+    }
+
+}
